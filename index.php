9f8425548f663361cb9b9a3568b55db04b174add
Naif <nayef7138@gmail.com>
2022-03-14 19:55:10 +0300

    add db class

--- index.php
+++ index.php
@@ -1,162 +1,31 @@
 <?php
-$dns = "mysql:host=localhost";
-$username = 'root';
-$password = '';
 
 
-
-
-class Database
-{
-
-
-  private $conn;
-  private $select_query;
-
-  public function __construct($db_name)
-  {
-    global $dns, $username, $password;
-
-    $this->conn = new PDO($dns, $username, $password, [
-      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
-      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-    ]);
-
-    $this->conn->exec("CREATE DATABASE IF NOT EXISTS `$db_name`; USE `$db_name`");
-  }
-
-  public function setTable(string $table_name, array $data)
-  {
-    $columns = "";
-    foreach ($data as $key => $value) {
-      $columns .= "`$key` $value ,";
-    }
-
-    $query = "CREATE TABLE IF NOT EXISTS `$table_name` ( 
-          `id` INT NOT NULL AUTO_INCREMENT ,
-          $columns
-          `is_active` INT DEFAULT 1 ,
-          PRIMARY KEY (`id`));
-    )";
-
-    $statement = $this->conn->prepare($query);
-    $statement->execute();
-  }
-
-
-  public function select(
-    string $table_name,
-    array $columns
-  ) {
-    $columns_name = implode(',', $columns);
-    $query = "SELECT $columns_name FROM $table_name ";
-
-
-
-    $statement = $this->conn->prepare($query);
-    $statement->execute();
-
-    $this->select_query = $query;
-
-    return $this;
-  }
-
-  public function where(array $conditions)
-  {
-    $where_condition = "";
-    foreach ($conditions as $item) {
-
-      // don't touch this code :)
-      if (strtoupper($item[1]) === "IN") {
-        $in_query = '';
-        foreach ($item[2] as $i) {
-          $in_query .= " '$i' ,";
-        }
-        $in_query = substr($in_query, 0, -1);
-
-        $where_condition .= "`{$item[0]}` {$item[1]} ($in_query) AND ";
-      } else {
-        $where_condition .= "`{$item[0]}` {$item[1]} '{$item[2]}' AND ";
-      }
-    }
-
-    $where_condition = substr($where_condition, 0, -4);
-
-    // print_r($where_condition);
-    // exit;
-
-    $query = $this->select_query .  " WHERE $where_condition ";
-
-
-    $statement = $this->conn->prepare($query);
-    $statement->execute();
-
-    print_r($statement->fetchAll());
-  }
-
-  public function insert(string $table_name, array $data)
-  {
-    $columns = '';
-    $values = '';
-
-    foreach ($data as $key => $value) {
-      $columns .= "`$key` ,";
-      $values .= "'$value' ,";
-    }
-
-    $columns = substr($columns, 0, -1);
-    $values = substr($values, 0, -1);
-
-    $query = "INSERT INTO `$table_name` ($columns) VALUES ($values)";
-    echo $query;
-
-    $statement = $this->conn->prepare($query);
-    $statement->execute();
-  }
-
-  public function update(string $table_name, int $id, array $data)
-  {
-    $params = [];
-
-    foreach ($data as $key => $value) {
-      array_push($params,  "`$key` = '$value'");
-    }
-
-    $sql  = "UPDATE $table_name SET " . implode(',', $params) .  " WHERE `id` = $id";
-
-    $statement = $this->conn->prepare($sql);
-    $statement->execute();
-  }
-
-  public function delete($table_name, $id)
-  {
-    $query = "DELETE FROM `$table_name` WHERE `id` = $id";
-
-    $statement = $this->conn->prepare($query);
-    $statement->execute();
-  }
-}
+require_once "db.php";
 
 $user = new Database('super-man');
 
-$user
-  ->select('erp2', [
-    "*"
-  ])
-  ->where([
-    ["name", "LIKE", "n%"],
-    ["age", ">", 1],
-    ["major", "IN", ['cs', 'it']]
-  ]);
+$result = $user
+  ->select('erp2, heros');
 
 
+print_r($result->where());
 
-// $user->setTable('erp2', [
-//   'name' => 'VARCHAR(255)',
-//   'age' => 'INT(12)',
-//   'major' => 'VARCHAR(12)',
-// ]);
+exit;
 
+$user->setTable('school', [
+  'name' => 'VARCHAR(255) NOT NULL',
+  'age' => 'INT(12) DEFAULT 5',
+]);
+
+
+try {
+  $table_header_items = $user->select('erp2', [
+    'id', 'name', 'age', 'major'
+  ])->where()[0];
+} catch (\Throwable $th) {
+  $table_header_items = [];
+}
 
 // $user->insert('erp2', [
 //   "name" => "new Naif",
@@ -170,3 +39,71 @@ $user
 // ]);
 
 // $user->delete('heros', 1);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Super team</title>
+
+  <link rel="stylesheet" href="css/output.css">
+</head>
+
+<body class="dark:bg-gray-500 p-8">
+  <div class="flex flex-col">
+    <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+      <div class="inline-block py-2 min-w-full sm:px-6 lg:px-8">
+        <div class="overflow-hidden shadow-md sm:rounded-lg">
+          <table class="min-w-full">
+            <thead class="bg-gray-50 dark:bg-gray-700">
+              <tr>
+                <!-- table head names -->
+                <?php foreach ($table_header_items as $key => $value) {
+                  echo "
+                    <th scope='col' class='py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400'> {$key} </th>
+                    ";
+                }
+                ?>
+                <td></td>
+              </tr>
+            </thead>
+
+            <tbody>
+              <?php foreach ($result as $item) {  ?>
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                  <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    <?php echo $item['id']; ?>
+                  </td>
+                  <td class="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
+                    <?php echo $item['name']; ?>
+                  </td>
+                  <td class="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
+                    <?php echo $item['age']; ?>
+                  </td>
+                  <td class="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
+                    <?php echo $item['major']; ?>
+                  </td>
+                  <td class="py-4 px-6 text-sm font-medium text-right whitespace-nowrap">
+                    <form action="delete.php" method="post">
+                      <input type="hidden" name="id" value="<?php echo $item['id']; ?>">
+                      <button name="delete_item" type="submit" class="text-red-600 dark:text-red-500 hover:underline">delete</button>
+                    </form>
+                  </td>
+                </tr>
+
+              <?php  }  ?>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+</body>
+
+</html>
